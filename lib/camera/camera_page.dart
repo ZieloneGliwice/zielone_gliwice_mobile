6c964bf768fe5e214cfabbb0e334ad6bcc8a76f1
Jakub Mirota <mirota.jakub@gmail.com>
2022-05-19 01:29:56 +0200

    Implements image cropping

---
+++ lib/camera/camera_page.dart
@@ -0,0 +1,79 @@
+import 'dart:ffi';
+import 'dart:io';
+
+import 'package:crop_image/crop_image.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+
+class CameraPage extends GetView<CameraPageController> {
+  const CameraPage({super.key});
+
+  static const String path = '/camera_page';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Zielone Gliwice'),
+      ),
+      body: controller.obx((XFile? imageFile) {
+        if (imageFile != null) {
+          return _cropping(imageFile);
+        } else {
+          return _noPhotoSelected();
+        }
+      },
+      onLoading: const Center(child: CircularProgressIndicator(),),
+      onError: (String? error) => Center(child:Text(error ?? '')),
+      onEmpty: _noPhotoSelected()),
+    );
+  }
+
+  Widget _noPhotoSelected() {
+    return const Center(child: Text('No photo selected'));
+  }
+
+  Widget _cropping(XFile imageFile) {
+    return Center(
+      child: Column(
+        children: [
+          const Text('Zaznacz obszar do analizy'),
+          CropImage(controller: controller.croppingController, image: Image.file(File(imageFile.path))),
+          OutlinedButton(onPressed: () => controller.finished(), child: const Text('Dalej'))
+        ],
+      ),
+    );
+  }
+}
+
+class CameraPageController extends GetxController with StateMixin<XFile> {
+  final ImagePicker _picker = ImagePicker();
+  final CropController croppingController = CropController();
+
+  @override
+  void onInit() {
+    _getFromCamera();
+    super.onInit();
+  }
+
+  Future<void> _getFromCamera() async {
+    change(null, status: RxStatus.loading());
+    try {
+      final XFile? selectedImage = await _picker.pickImage(source: ImageSource.camera);
+      change(selectedImage, status: RxStatus.success());
+    } catch (error) {
+      change(null, status: RxStatus.error(error.toString()));
+    }
+  }
+
+  Future<void> finished() async {
+    final context = Get.context;
+
+    if (context != null) {
+      final image = await croppingController.croppedImage();
+    } else {
+      change(null, status: RxStatus.error('Something went wrong please try again'));
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'camera/camera_page.dart';
+import 'camera/camera_page_binding.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Zielone Gliwice',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(),
+      getPages: [
+        GetPage(name: MyHomePage.path, page: () => const MyHomePage()),
+        GetPage(name: CameraPage.path, page: () => const CameraPage(), binding: CameraPageBind())
+      ],
+      initialRoute: MyHomePage.path,
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({super.key});
+
+  static const String path = '/home';
 
   @override
   Widget build(BuildContext context) {
@@ -29,8 +39,11 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Zielone Gliwice'),
       ),
-      body: const Center(
-        child: Text('Zielone Gliwice'),
+      body: Center(
+        child: OutlinedButton(
+            onPressed: () => Get.toNamed(CameraPage.path),
+            child: const Text('Dodaj nowe zdjęcie')
+        ),
       ),
     );
   }
