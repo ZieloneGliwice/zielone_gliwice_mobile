8859623bad2b4b2062654bb5749a990b1415143b
Jakub Mirota <mirota.jakub@gmail.com>
2022-05-19 23:14:21 +0200

    Adds localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'internationalization/translations.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Zielone Gliwice',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
+      translations: ApplicationTranslations(),
+      locale: Get.deviceLocale,
+      fallbackLocale: const Locale('en', 'US'),
       home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Zielone Gliwice'),
+        title: Text('my_trees_title'.tr),
       ),
       body: const Center(
         child: Text('Zielone Gliwice'),
